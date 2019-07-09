d904e710ea2d444db44c22fa1fc8ce2c6a635f8b
HP <https://github.com/Angel-cec3/Android-Studio>
2019-07-09 23:53:41 +0530

    Initial commit

---
+++ app/src/main/java/app/cec/networkingsample/MainActivity.java
@@ -0,0 +1,93 @@
+package app.cec.networkingsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResult;
+    EditText etUrl;
+    Button btnGo;
+    String url="https://www.google.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult=findViewById(R.id.tv_result);
+        etUrl=findViewById(R.id.et_url);
+        btnGo=findViewById(R.id.btn_go);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                url=etUrl.getText().toString();
+                if (!url.isEmpty()){
+                    doHttpCall();
+                }else{
+                    url="https://www.google.com";
+                    doHttpCall();
+                }
+            }
+        });
+    }
+
+    public void doHttpCall(){
+        new HttpTask(url).execute();
+
+    }
+
+    class HttpTask extends AsyncTask{
+        String url;
+
+        HttpTask(String url){//constructor
+            this.url=url;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            tvResult.setText("Loading");
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String line="";//declared as global variable since line is in try block and cannot be returned unless it is a global variable
+            try{
+                URL url=new URL(this.url);
+                HttpURLConnection connection=(HttpURLConnection)url.openConnection();
+                connection.setRequestMethod("GET");
+                InputStream inputStream=connection.getInputStream();
+                InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+                BufferedReader bufferdReader=new BufferedReader(inputStreamReader);
+                line=((BufferedReader) bufferdReader).readLine();
+            }catch (Exception e){
+                e.printStackTrace();
+                line="ERROR OCCURED";
+
+            }
+
+
+            return line;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            tvResult.setText((String)o);
+        }
+    }
+}
